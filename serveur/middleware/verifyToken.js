f9765f55a8b1cc3786e48c6b66785cf4a73c233d
HABTI Mohammed <m.habti@FADUPS.ma>
2024-05-06 18:08:45 +0100

    test

---
+++ serveur/middleware/verifyToken.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    // Get JWT token from cookie
+    const token = req.cookies.jwt;
+
+    if (!token) {
+        return res.status(401).json({ message: 'JWT requis' });
+    }
+
+    // Verify JWT
+    jwt.verify(token, 'secret_key', (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: 'JWT invalide' });
+        }
+
+        // Store decoded JWT payload in req.user
+        req.user = decoded;
+        console.log("f_mdlwr_vrfy: ",req.user.type_User)
+
+        next();
+    });
+};
+
+module.exports = verifyToken;
---
+++ serveur/routes/auth.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+
+//=======================================
+const mysql = require('mysql');
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root', // Your MySQL username
+    password: null, // Your MySQL password
+    database: 'fadtestdb', // Name of the database you created
+})
+
+
+router.post('/login', (req, res) => {
+    const { name, password } = req.body;
+
+    pool.getConnection((err, connection) => {
+        if (err) {
+            return res.status(500).json({ message: 'Erreur de connexion à la base de données' });
+        }
+
+        // const query = 'SELECT id_User, type_User FROM Users WHERE login_User = ? AND password_User = ?';
+        const query = 'SELECT * FROM user WHERE login_User = ? AND password_User = ?';
+        connection.query(query, [name, password], (err, results) => {
+            connection.release();
+            console.log("logintst:result",results)
+
+            if (err) {
+                return res.status(500).json({ message: 'Erreur lors de la récupération des données utilisateur' });
+            }
+
+            if (results.length === 0) {
+                return res.status(401).json({ message: 'Identifiants incorrects' });
+            }
+
+            const { id_User, type_User } = results[0];
+            // Generate JWT with user's id and type
+            const token = jwt.sign({ id_User, type_User }, 'secret_key');
+
+            // Set JWT as a cookie
+            res.cookie('jwt', token, { httpOnly: true, maxAge: 86400000 }); // Expires in 24 hours
+            res.json({ message: 'Connecté avec succès' });
+        });
+    });
+});
+
+
+
+module.exports = router;
+
+// module.exports = { router, verifyToken };
+
+
+//=======================================
+
+
+// router.post('/login', (req, res) => {
+//     const { name, password } = req.body;
+
+//     pool.getConnection((err, connection) => {
+//         if (err) {
+//             return res.status(500).json({ message: 'Erreur de connexion à la base de données' });
+//         }
+//         // const query = 'SELECT id_User, type_User FROM Users WHERE login_User = ? AND password_User = ?';
+//         const query = 'SELECT * FROM test_user WHERE name = ? AND password = ?';
+//         connection.query(query, [name, password], (err, results) => {
+//             connection.release();
+//             console.log(results)
+//             if (err) {
+//                 return res.status(500).json({ message: 'Erreur lors de la récupération des données utilisateur' });
+//             }
+//             if (results.length === 0) {
+//                 return res.status(401).json({ message: 'Identifiants incorrects' });
+//             }
+//             const { id_test_user} = results[0];
+//             const token = jwt.sign({ id_test_user }, 'secret_key');
+//             res.cookie('jwt', token, { httpOnly: true, maxAge: 86400000 }); // Expires in 24 hours
+//             // const { id_User, type_User } = results[0];
+//             // Generate JWT with user's id and type
+//             // const token = jwt.sign({ id_User, type_User }, 'secret_key');
+//             // Set JWT as a cookie
+//             // res.cookie('jwt', token, { httpOnly: true, maxAge: 86400000 }); // Expires in 24 hours
+//             res.json({ message: 'Connecté avec succès' });
+//         });
+//     });
+// });
