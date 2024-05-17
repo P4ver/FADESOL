9c20ff69bd08deca654a3ccfc811cb49ee0ba873
P4ver <s.pales20@gmail.com>
2024-05-17 10:40:42 +0100

    last versin of login status works

--- serveur/Controller/userController.js
+++ serveur/Controller/userController.js
@@ -40,29 +40,18 @@ const ajouterUser = (req, res)=>{
     })
 }
 
-// const modifierUser = (req, res)=>{
-//     pool.getConnection((err, connection)=>{
-//         if (err) throw err
-//         console.log("connection as id", connection.threadId)
-//         const { id_User, login_User, password_User, nom_User, prenom_User, tel_User, note_User, type_User} = req.body
-//         connection.query("UPDATE user SET login_User = ?, password_User = ?, nom_User = ?, prenom_User = ?, tel_User = ?, note_User = ?, type_User = ?  WHERE id_User = ?", [ login_User, password_User, nom_User, prenom_User, tel_User, note_User, type_User, id_User],(err, rows)=>{
-//             connection.release()
-//             if (err) throw err
-//             res.send("Les données ont été mises à jour.")
-//         })
-//     })
-//}
+
 
 const modifierUser = (req, res) => {
     pool.getConnection((err, connection) => {
         if (err) throw err;
         console.log("connection as id", connection.threadId);
-        const { login_User, password_User, nom_User, prenom_User, tel_User, note_User, type_User, email_User} = req.body
+        const { login_User, password_User, nom_User, prenom_User, tel_User, note_User, type_User, email_User, status} = req.body
         const { id } = req.params; // Assuming id is the name of the parameter in the route
 
         connection.query(
-            "UPDATE user SET login_User = ?, password_User = ?, nom_User = ?, prenom_User = ?, tel_User = ?, note_User = ?, type_User = ?, email_User = ?  WHERE id_User = ?",
-            [ login_User, password_User, nom_User, prenom_User, tel_User, note_User, type_User, email_User, id],
+            "UPDATE user SET login_User = ?, password_User = ?, nom_User = ?, prenom_User = ?, tel_User = ?, note_User = ?, type_User = ?, email_User = ?, status = ?  WHERE id_User = ?",
+            [ login_User, password_User, nom_User, prenom_User, tel_User, note_User, type_User, email_User, status, id],
             (err, rows) => {
                 connection.release();
                 if (err) throw err;
