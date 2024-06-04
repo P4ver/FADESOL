773491aa7266761a3a5474b5473b004a0ff8a72d
P4ver <s.pales20@gmail.com>
2024-06-04 13:55:01 +0100

    test

--- FrontEnd_Test/src/component/sideBare.jsx
+++ FrontEnd_Test/src/component/sideBare.jsx
@@ -36,7 +36,7 @@ const SideBare = () => {
      
         <div className="flex flex-col items-center px-6 py-3 space-y-2 bg-gray-700 rounded-lg mt-4">
           <div className="flex items-center justify-center w-12 h-12 bg-gray-500 rounded-full text-xl font-bold">
-            {userInitials}
+            {/* {userInitials} */}
           </div>
           <div className="text-lg text-center">
             {/* Welcome back, <br /> {foundUser.nom_User}  {foundUser.prenom_User} */}
