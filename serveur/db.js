9a2af1dd15287fc3a214a6b2ff70815915184ef3
P4ver <s.pales20@gmail.com>
2024-06-04 10:21:19 +0100

    test

--- serveur/db.js
+++ serveur/db.js
@@ -2,10 +2,17 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root', // Your MySQL username
-    password: null, // Your MySQL password
-    database: 'fadesoldb', // Name of the database you created
+    host: 'bdibawrkccer658nc2x3-mysql.services.clever-cloud.com',
+    user: 'uwdlyklpge60bjid', // Your MySQL username
+    password: 'a6ETVhHQPzWDQjxOyyjG', // Your MySQL password
+    database: 'bdibawrkccer658nc2x3', // Name of the database you created
 });
+// const pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: 'localhost',
+//     user: 'root', // Your MySQL username
+//     password: null, // Your MySQL password
+//     database: 'fadesoldb', // Name of the database you created
+// });
 
 module.exports = pool;
