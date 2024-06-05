58fab8fd1659d22606ea515390812d744cb2fe33
P4ver <s.pales20@gmail.com>
2024-06-05 14:44:46 +0100

    dd

--- FrontEnd_Test/vite.config.js
+++ FrontEnd_Test/vite.config.js
@@ -1,24 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy all requests to your backend
-      '/achatempo': {
-        target: 'https://fadesol-puoc.vercel.app',
-        changeOrigin: true,
-        secure: false, // Use this option if you are using self-signed certificates
-      },
-    },
-  },
-});
+})
