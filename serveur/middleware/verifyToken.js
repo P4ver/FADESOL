472ace972e538bbc48dbf0bbc7df5e3c3135c69f
P4ver <s.pales20@gmail.com>
2024-06-05 09:26:34 +0100

    BE online1

--- serveur/middleware/verifyToken.js
+++ serveur/middleware/verifyToken.js
@@ -1,6 +1,37 @@
 const jwt = require('jsonwebtoken');
 
 // Middleware function for verifying JWT token
+// const verifyToken = (req, res, next) => {
+//     const token = req.cookies.jwt;
+
+//     if (!token) {
+//         return res.status(401).json({ message: 'JWT requis' });
+//     }
+
+//     jwt.verify(token, 'secret_key', (err, decoded) => {
+//         if (err) {
+//             return res.status(403).json({ message: 'JWT invalide' });
+//         }
+
+//         req.user = decoded;
+//         next(); // Proceed to the next middleware
+//     });
+// };
+
+// // Middleware function for authorizing user roles
+// const authorizeRole = (roles) => {
+//     return (req, res, next) => {
+//         const role = req.user.type_User;
+
+//         if (roles.includes(role)) {
+//             next(); // Allow access if user role is in the allowed roles
+//         } else {
+//             return res.status(403).json({ message: 'Accès non autorisé' });
+//         }
+//     };
+// };
+const jwt = require('jsonwebtoken');
+
 const verifyToken = (req, res, next) => {
     const token = req.cookies.jwt;
 
@@ -17,8 +48,6 @@ const verifyToken = (req, res, next) => {
         next(); // Proceed to the next middleware
     });
 };
-
-// Middleware function for authorizing user roles
 const authorizeRole = (roles) => {
     return (req, res, next) => {
         const role = req.user.type_User;
@@ -33,7 +62,6 @@ const authorizeRole = (roles) => {
 
 module.exports = { verifyToken, authorizeRole };
 
-// const jwt = require('jsonwebtoken');
 
 // const verifyToken = (req, res, next) => {
 //     // Get JWT token from cookie
