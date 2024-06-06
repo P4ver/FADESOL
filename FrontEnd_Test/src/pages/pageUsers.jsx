b53e44d0ac0b57e0dc353ed001c748d084714f72
P4ver <s.pales20@gmail.com>
2024-06-06 11:00:15 +0100

    tes

--- FrontEnd_Test/src/pages/pageUsers.jsx
+++ FrontEnd_Test/src/pages/pageUsers.jsx
@@ -10,7 +10,8 @@ const PageUsers = () => {
   const findUser = userData.userData.find(user => user.login_User === username);
   
   const [userRole, setUserRole] = useState(localStorage.getItem("userRole"));
-
+  console.log('===userpage===========>')
+  console.log("TST !", userData)
   useEffect(() => {
     if (findUser) {
       const role = findUser.type_User;
@@ -20,21 +21,19 @@ const PageUsers = () => {
     }
   }, [findUser]);
 
-  if (!findUser) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-white p-10 border border-gray-300 rounded-lg shadow-lg text-center">
-          <h1 className="text-2xl font-bold mb-4">User not found</h1>
-        </div>
-      </div>
-    );
-  }
-  console.log('===userpage===========>')
-  console.log("TST !", userData)
+  // if (!findUser) {
+  //   return (
+  //     <div className="flex items-center justify-center h-screen">
+  //       <div className="bg-white p-10 border border-gray-300 rounded-lg shadow-lg text-center">
+  //         <h1 className="text-2xl font-bold mb-4">User not found</h1>
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
   return (
     <div>
-      <TableTest />
-      {/* {userRole === 'Super Admin' || userRole === 'Admin' ? (
+      {userRole === 'Super Admin' || userRole === 'Admin' ? (
         <TableTest />
       ) : (
         <div className="flex items-center justify-center h-screen">
@@ -42,7 +41,7 @@ const PageUsers = () => {
             <h1 className="text-2xl font-bold mb-4">You do not have access to this page.</h1>
           </div>
         </div>
-      )} */}
+      )}
     </div>
   );
 };
