3e813b6598b908611e8f0fdc87c508a6995853ab
P4ver <s.pales20@gmail.com>
2024-06-09 16:10:53 +0100

    testg

--- FrontEnd_Test/src/component/navBar.jsx
+++ FrontEnd_Test/src/component/navBar.jsx
@@ -15,8 +15,7 @@ const NavBar = () => {
   
   console.log("navBar: userData.map(usr.login_User:):",userState.userData.map(usr=>usr.login_User))
   const matchedUser = userState.userData.find(usr => usr.login_User == user.username );
-  console.log("=>from navBar: the role : ", matchedUser);
-  console.log("=>from navBar: the role : ", matchedUser);
+  console.log("navBar: the role : ", matchedUser.type_User);
 
   const userId = user.id; // Or any other unique identifier
   
