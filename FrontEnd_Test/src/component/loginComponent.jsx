db5aafeee212eb7c7de4fee8136065b23f174cdd
P4ver <s.pales20@gmail.com>
2024-06-09 18:33:16 +0100

    ds

--- FrontEnd_Test/src/component/loginComponent.jsx
+++ FrontEnd_Test/src/component/loginComponent.jsx
@@ -1,16 +1,13 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { FaLock, FaUser } from 'react-icons/fa';
-import { useNavigate  } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
 import axios from 'axios';
 import { loginSuccess, loginFailure } from '../store/authActions';
-import { Header } from './oldComponent/Header';
-import { API_BASE_URL } from '../apiConfig';
-  
+import logo from '../pictures/logo.png';
+import ReCAPTCHA from 'react-google-recaptcha';
+
 const LoginComponent = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -18,111 +15,137 @@ const LoginComponent = () => {
     name: '',
     password: '',
   });
+  const [recaptchaToken, setRecaptchaToken] = useState('');
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const response = await axios.post('https://fadesol-puoc.vercel.app/auth/login', formData, {
-//         withCredentials: true,
-//       });
-//       console.log('Login successful:', response.config.data);
-//       localStorage.setItem('isAuthenticated', 'true');
-// const objtext = response.config.data;
-// const obj = JSON.parse(objtext)
-//       dispatch(loginSuccess(obj)); // Assuming response.data contains user info
-//       navigate('/dashboard'); // Assuming navigate is defined somewhere
-//     } catch (error) {
-//       console.error('Login failed:', error);
-//       dispatch(loginFailure(error.message));
-//       toast.error('La connexion a échoué. Veuillez vérifier vos informations d\'identification.');
+  const handleRecaptchaChange = (token) => {
+    setRecaptchaToken(token);
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!recaptchaToken) {
+      toast.error('Veuillez compléter la vérification reCAPTCHA.');
+      return;
+    }
+    try {
+      const response = await axios.post(`${API_BASE_URL}/auth/login`, { ...formData, recaptchaToken }, {
+        withCredentials: true,
+      });
+      console.log('Login successful:', response.config.data);
+      localStorage.setItem('isAuthenticated', 'true');
+      const objtext = response.config.data;
+      const obj = JSON.parse(objtext);
+      dispatch(loginSuccess(obj)); // Assuming response.data contains user info
+      navigate('/dashboard'); // Assuming navigate is defined somewhere
+    } catch (error) {
+      console.error('Login failed:', error);
+      dispatch(loginFailure(error.message));
+      toast.error('La connexion a échoué. Veuillez vérifier vos informations d\'identification.');
+    }
+  };
 
-//     }
-//   };
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    const response = await axios.post(`${API_BASE_URL}/auth/login`, formData, {
-      withCredentials: true,
-    });
-    console.log('Login successful:', response.config.data);
-    localStorage.setItem('isAuthenticated', 'true');
-    const objtext = response.config.data;
-    const obj = JSON.parse(objtext)
-    dispatch(loginSuccess(obj)); // Assuming response.data contains user info
-    navigate('/dashboard'); // Assuming navigate is defined somewhere
-  } catch (error) {
-    console.error('Login failed:', error);
-    dispatch(loginFailure(error.message));
-    toast.error('La connexion a échoué. Veuillez vérifier vos informations d\'identification.');
-  }
-};
   const handleSignUpClick = () => {
     navigate('/register');
   };
 
   return (
-    <div className="log-main">
-      
-      <Header title="Se connecter" />
-      <form onSubmit={handleSubmit}>
-        <div className="login-section">
-          <label htmlFor="name">Nom d'utilisateur</label>
-          <input
-            id="login"
-            className="login-input"
-            type="text"
-            placeholder="Saisir votre nom"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-          <FaUser className="icon" />
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="w-32 mx-auto">
+          <img src={logo} alt="Logo" />
         </div>
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
 
-        <div className="password-section">
-          <label htmlFor="password">Mot de passe</label>
-          <input
-            className="password-input"
-            type="password"
-            placeholder="Saisir votre mot de passe"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-          <FaLock className="icon" />
-        </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm bg-white p-8 shadow-lg rounded-lg">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+              Nom d'utilisateur
+            </label>
+            <div className="mt-2 w-[300px]">
+              <input
+                id="login"
+                type="text"
+                placeholder="Saisir votre nom"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-        <div className="log-button">
-          <button type="submit">Me connecter</button>
-          <a href="#">Mot de passe oublié ?</a>
-        </div>
+          <div className="mt-4">
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+              <div className="text-sm">
+                <a href="#" className="font-semibold text-customBlue hover:text-indigo-500">
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2 w-[300px]">
+              <input
+                id="password"
+                type="password"
+                placeholder="Saisir votre mot de passe"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-        <div className="sign-up-button">
-          <a href="#">Je n'ai pas de compte</a>
-          <button type="button" onClick={handleSignUpClick}>
-            M'inscrire
-          </button>
-        </div>
-      </form>
-      <ToastContainer 
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
+          <div className="mt-4">
+          <ReCAPTCHA
+  sitekey="6LfFO_MpAAAAAOIgO1O8KmM7yBO6DqROTLdNf8zA"
+  onChange={handleRecaptchaChange}
+/>
+
+
+          </div>
+
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-customGreen px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{' '}
+          <a href="#" className="font-semibold leading-6 text-customBlue hover:text-indigo-500" type="button" onClick={handleSignUpClick}>
+            Register Now
+          </a>
+        </p>
+      </div>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
       />
     </div>
   );
@@ -130,163 +153,69 @@ const handleSubmit = async (e) => {
 
 export default LoginComponent;
 
+//==============================================================================================================================
 
 
-
-// import React, { useState , useEffect} from 'react';
-// import { useNavigate  } from 'react-router-dom'; 
-// import { FaLock, FaUser } from "react-icons/fa";
-// import axios from 'axios';
-
-
-// import './LoginForm.css'
-// import { Header } from "./oldComponent/Header";
-
-
-// const LoginComponent = ({handleLogin })=>{
-
-//     const [formData, setFormData] = useState({
-//         name: '',
-//         password: ''
-//       });
-  
-
-
-//       const navigate = useNavigate();
-//       const handleChange = (e) => {
-//         setFormData({ ...formData, [e.target.name]: e.target.value });
-//       };
-
-//       const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         try {
-//           const response = await axios.post('https://fadesol-puoc.vercel.app/auth/login', formData, {
-//             withCredentials: true,
-//           });
-//           console.log('Login successful:', response.data);
-//           localStorage.setItem('isAuthenticated', 'true');
-
-//           navigate('/test');
-//         } catch (error) {
-//           console.error('Login failed:', error);
-//         }
-//       };
-
-
-//     const handleSignUpClick = () => {
-//         navigate('/register');
-//     };
-
-//     return(
-//     <div className='log-main'>
-//         <Header title='Se connecter'/>
-//         <form onSubmit={handleSubmit}>
-//             <div className="login-section">
-//                 <label htmlFor="name">Nom d'utilisateur</label>
-//                 <input 
-//                   id="login"
-//                   className='login-input'
-//                   type='text'
-//                   placeholder='Saisir votre nom'
-//                   name='name'
-//                   value={formData.name}
-//                   onChange={handleChange}
-//                   required
-//                   />
-//                   <FaUser className="icon"/>
-//             </div>
-
-//             <div className="password-section">
-//                 <label htmlFor="password">Mot de passe</label>
-//                 <input 
-//                     className='password-input'
-//                     type='password'
-//                     placeholder='Saisir votre mot de passe'
-//                     name='password'
-//                     value={formData.password}
-//                     onChange={handleChange}
-//                     required
-//                     />
-//                     <FaLock className="icon"/>
-//             </div>
-
-//             <div className="log-button">
-//                 <button type="submit">Me connecter</button>
-//                 <a href="#">Mot de passe oublié ?</a>
-//             </div>
-
-            
-//             <div className="sign-up-button">
-//               <a href="#">Je n'ai pas de compte</a>
-//                     <button type="button" onClick={handleSignUpClick}>M'inscrire</button>
-//             </div>
-//     </form>
-//     </div>
-
-//     )
-// }
-
-// export default LoginComponent;
-
-//importants login component with redux
-
-
-// // Login.js
 // import React, { useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// // import { loginAsync, selectAuth, loginSuccess } from './authSlice';
-// import { loginAsync, selectAuth, loginSuccess } from '../store/authSlice';
-// import { FaLock, FaUser } from "react-icons/fa";
-// import { useNavigate } from 'react-router-dom';
-// import './LoginForm.css'
+// import { useDispatch } from 'react-redux';
+// import { FaLock, FaUser } from 'react-icons/fa';
+// import { useNavigate  } from 'react-router-dom';
+// import { toast, ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
+
+// import axios from 'axios';
+// import { loginSuccess, loginFailure } from '../store/authActions';
+// import { Header } from './oldComponent/Header';
+// import { API_BASE_URL } from '../apiConfig';
+  
 // const LoginComponent = () => {
 //   const dispatch = useDispatch();
 //   const navigate = useNavigate();
-//   const { loading, error } = useSelector(selectAuth);
-
 //   const [formData, setFormData] = useState({
 //     name: '',
-//     password: ''
+//     password: '',
 //   });
 
 //   const handleChange = (e) => {
 //     setFormData({ ...formData, [e.target.name]: e.target.value });
 //   };
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     dispatch(loginAsync(formData))
-//       .then(() => {
-//         localStorage.setItem('isAuthenticated', 'true');
-//         // Navigate to '/test' upon successful login
-//         console.log("=============>", loginSuccess(formData))
-
-//         dispatch(loginSuccess(formData)); // You might want to pass user data instead of formData
-        
-//         navigate('/test');
-//       })
-//       .catch((error) => {
-//         console.error('Login failed:', error);
-//       });
-//   };  
-
+// const handleSubmit = async (e) => {
+//   e.preventDefault();
+//   try {
+//     const response = await axios.post(`${API_BASE_URL}/auth/login`, formData, {
+//       withCredentials: true,
+//     });
+//     console.log('Login successful:', response.config.data);
+//     localStorage.setItem('isAuthenticated', 'true');
+//     const objtext = response.config.data;
+//     const obj = JSON.parse(objtext)
+//     dispatch(loginSuccess(obj)); // Assuming response.data contains user info
+//     navigate('/dashboard'); // Assuming navigate is defined somewhere
+//   } catch (error) {
+//     console.error('Login failed:', error);
+//     dispatch(loginFailure(error.message));
+//     toast.error('La connexion a échoué. Veuillez vérifier vos informations d\'identification.');
+//   }
+// };
 //   const handleSignUpClick = () => {
 //     navigate('/register');
-// };
-
+//   };
 
 //   return (
-//     <div className='log-main'>
+//     <div className="log-main">
+      
+//       <Header title="Se connecter" />
 //       <form onSubmit={handleSubmit}>
 //         <div className="login-section">
 //           <label htmlFor="name">Nom d'utilisateur</label>
 //           <input
 //             id="login"
-//             className='login-input'
-//             type='text'
-//             placeholder='Saisir votre nom'
-//             name='name'
+//             className="login-input"
+//             type="text"
+//             placeholder="Saisir votre nom"
+//             name="name"
 //             value={formData.name}
 //             onChange={handleChange}
 //             required
@@ -297,10 +226,10 @@ export default LoginComponent;
 //         <div className="password-section">
 //           <label htmlFor="password">Mot de passe</label>
 //           <input
-//             className='password-input'
-//             type='password'
-//             placeholder='Saisir votre mot de passe'
-//             name='password'
+//             className="password-input"
+//             type="password"
+//             placeholder="Saisir votre mot de passe"
+//             name="password"
 //             value={formData.password}
 //             onChange={handleChange}
 //             required
@@ -309,19 +238,33 @@ export default LoginComponent;
 //         </div>
 
 //         <div className="log-button">
-//           <button type="submit" disabled={loading}>Me connecter</button>
-//           {error && <div>{error}</div>}
+//           <button type="submit">Me connecter</button>
 //           <a href="#">Mot de passe oublié ?</a>
 //         </div>
 
 //         <div className="sign-up-button">
 //           <a href="#">Je n'ai pas de compte</a>
-//           <button type="button" onClick={handleSignUpClick}>M'inscrire</button>
-
+//           <button type="button" onClick={handleSignUpClick}>
+//             M'inscrire
+//           </button>
 //         </div>
 //       </form>
+//       <ToastContainer 
+//           position="top-center"
+//           autoClose={5000}
+//           hideProgressBar={false}
+//           newestOnTop={false}
+//           closeOnClick
+//           rtl={false}
+//           pauseOnFocusLoss
+//           draggable
+//           pauseOnHover
+//           theme="light"
+//       />
 //     </div>
 //   );
 // };
 
 // export default LoginComponent;
+
+
