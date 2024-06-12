2a36122c0bde1b82a301b24b50b4e4f0a4f30f42
salmabourizi-dev <bourizisalma5@gmail.com>
2024-06-12 17:38:34 +0100

    msg

--- FrontEnd_Test/src/App.jsx
+++ FrontEnd_Test/src/App.jsx
@@ -1,3 +1,151 @@
+// import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+// import React from 'react';
+// import { Provider } from 'react-redux';
+// import store from './store/store';
+// // import Dashboard from './component/dashboard';
+// import Register from './component/register';
+// import './App.css';
+// import LoginComponent from './component/loginComponent';
+// import PrivateRoute from './component/PrivateRoute';
+// import ErrorPage from './component/ErrorPage';
+// import PageUsers from '../src/pages/pageUsers';
+// import PageDashboard from '../src/pages/pageDashboard';
+// import Layout from './component/layout';
+// import PageProducts from './pages/pageProducts';
+// import Entree from './component/entree';
+// import Livraisons from './component/Livraison';
+// import Sortie from './component/sortie';
+// import Dashboard from "./component/dashboard"
+// import ListeDemandes from './component/listeDemande';
+// import DeliveredItemsPage from './component/livrePage';
+// import { SnackbarProvider } from 'notistack';
+
+// import Profile from './component/profile';
+
+
+// function App() {
+//   return (
+//     <>
+//       <Provider store={store}>
+        
+//       <SnackbarProvider maxSnack={3}>
+//         <BrowserRouter>
+//           <Routes>
+//             <Route path="/login" element={<LoginComponent />} />
+//             <Route path="/" element={<LoginComponent />} />
+//             <Route path="/register" element={<Register />} />
+//             <Route path="/*" element={<ErrorPage />} />
+//             <Route element={<PrivateRoute />}>
+//               <Route element={<Layout />}>
+//                 <Route path="/users" element={<PageUsers />} />
+//                 {/* <Route path="/" element={<PageDashboard />} exact /> */}
+//                 <Route path="/products" element={<PageProducts/>} />
+//                 <Route path="/entree" element={<Entree/>} />
+//                 <Route path="/sortie" element={<Sortie/>} />
+//                 <Route path="/profile" element={<Profile/>} />
+//                 {/* <Route path="/Livraison" element={<Livraisons/>} /> */}
+//                 <Route path="/dashboard" element={<Dashboard/>} />
+//                 <Route path="/liste-demandes" element={<ListeDemandes />} />
+//                 <Route path="/livraison" element={<DeliveredItemsPage />} />
+               
+
+//               </Route>
+//             </Route>
+         
+//           </Routes>
+     
+//         </BrowserRouter>
+//         </SnackbarProvider>
+//       </Provider>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+// // import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+// // import React from 'react';
+// // import { Provider } from 'react-redux';
+// // import store from './store/store';
+// // import Dashboard from './component/dashboard';
+// // import Register from './component/register';
+// // import './App.css';
+// // import LoginComponent from './component/loginComponent';
+// // import PrivateRoute from './component/PrivateRoute';
+// // import ErrorPage from './component/ErrorPage';
+// // import PageUsers from './component/pages/pageusers';
+// // import Layout from './component/layout'; // Import the Layout component
+// // import pageDashboard from './component/pages/pageDashboard';
+// // function App() {
+// //   return (
+// //     <>
+// //       <Provider store={store}>
+// //         <BrowserRouter>
+// //           <Routes>
+// //             <Route path="/login" element={<LoginComponent />} />
+// //             <Route path="/register" element={<Register />} />
+// //             <Route path="/*" element={<ErrorPage />} /
+// //             <Route element={<PrivateRoute />}>
+// //               <Route element={<Layout />}>
+// //                 <Route path="/users" element={<PageUsers />} />
+// //                 <Route path="/test" element={<pageDashboard />}  />
+// //                 {/* <Route path="/test" element={<Dashboard />} exact /> */}
+// //               </Route>
+// //             </Route>
+// //           </Routes>
+// //         </BrowserRouter>
+// //       </Provider>
+// //     </>
+// //   );
+// // }
+// // export default App;
+// // import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+// //   import React from 'react';
+// //   import { Provider } from 'react-redux';
+// //   import store from './store/store'
+// //   import Dashboard from './component/dashboard'
+// //   import Register from './component/register'
+// //   import './App.css'
+// //   import LoginComponent from './component/loginComponent';
+// //   import PrivateRoute from './component/PrivateRoute';
+// //   import ErrorPage from './component/ErrorPage';
+// // import PageUsers from './component/pages/pageusers';
+// // // import pageDashboard from './component/pages/pageDashboard';
+// //   function App() {
+// //     return (
+// //       <>
+// //       <Provider store={store}>
+// //         <BrowserRouter>
+// //           <Routes>
+// //              <Route path="/login" element={<LoginComponent/>} />
+// //              <Route element={<PrivateRoute />}>
+// //                 <Route element={<Dashboard/>} path='/test' exact/>
+// //              </Route>
+// //              <Route path="/register" element={<Register/>} />
+// //              <Route path="/*" element={<ErrorPage/>} />
+// //              <Route path="/users" element={<PageUsers/>} />
+// //           </Routes>
+// //         </BrowserRouter>
+// //       </Provider>
+// //       </>
+// //     )
+// //   }
+// //   export default App
+//     {/* <Route element={<pageDashboard/>} path='/test' exact/> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
@@ -9,24 +157,30 @@ import LoginComponent from './component/loginComponent';
 import PrivateRoute from './component/PrivateRoute';
 import ErrorPage from './component/ErrorPage';
 import PageUsers from '../src/pages/pageUsers';
-import PageDashboard from '../src/pages/pageDashboard';
 import Layout from './component/layout';
 import PageProducts from './pages/pageProducts';
 import Entree from './component/entree';
-import Livraisons from './component/Livraison';
 import Sortie from './component/sortie';
 import Dashboard from "./component/dashboard"
-import ListeDemandes from './component/listeDemande';
+import PageDashboard from '../src/pages/pageDashboard';
+import Livraisons from './component/Livraison';
+// import ListeDemandes from './component/listeDemande';
 import DeliveredItemsPage from './component/livrePage';
-import { SnackbarProvider } from 'notistack';
-
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import Profile from './component/profile';
+import PageListeDemande from './pages/pageListeDemande';
+import PageLivraison from './pages/pageLivraison';
+import PageEntree from './pages/pageEntree';
+import PageSortie from './pages/pageSortie';
+disableReactDevTools()
+// if (process.env.NODE_ENV === 'production') {
+//   disableReactDevTools();
+// }
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        
-      <SnackbarProvider maxSnack={3}>
         <BrowserRouter>
           <Routes>
             <Route path="/login" element={<LoginComponent />} />
@@ -36,23 +190,21 @@ function App() {
             <Route element={<PrivateRoute />}>
               <Route element={<Layout />}>
                 <Route path="/users" element={<PageUsers />} />
-                {/* <Route path="/" element={<PageDashboard />} exact /> */}
                 <Route path="/products" element={<PageProducts/>} />
-                <Route path="/entree" element={<Entree/>} />
-                <Route path="/sortie" element={<Sortie/>} />
-                {/* <Route path="/Livraison" element={<Livraisons/>} /> */}
+                {/* <Route path="/entree" element={<Entree/>} /> */}
+                <Route path="/entree" element={<PageEntree/>} />
+                {/* <Route path="/sortie" element={<Sortie/>} /> */}
+                <Route path="/sortie" element={<PageSortie/>} />
+                <Route path="/profile" element={<Profile/>} />
                 <Route path="/dashboard" element={<Dashboard/>} />
-                <Route path="/liste-demandes" element={<ListeDemandes />} />
-                <Route path="/livraison" element={<DeliveredItemsPage />} />
-               
-
+                {/* <Route path="/liste-demandes" element={<ListeDemandes />} /> */}
+                <Route path="/liste-demandes" element={<PageListeDemande />} />
+                {/* <Route path="/livraison" element={<DeliveredItemsPage />} /> */}
+                <Route path="/livraison" element={<PageLivraison />} />
               </Route>
             </Route>
-         
           </Routes>
-     
         </BrowserRouter>
-        </SnackbarProvider>
       </Provider>
     </>
   );
--- FrontEnd_Test/src/component/sideBare.jsx
+++ FrontEnd_Test/src/component/sideBare.jsx
@@ -8,22 +8,24 @@
 // import { AiOutlineProduct } from "react-icons/ai";
 // import { SlBasket, SlArrowDown, SlArrowUp } from "react-icons/sl";
 // import { RxDashboard } from "react-icons/rx";
+// import { CgProfile } from "react-icons/cg";
 
 // const SideBare = () => {
 //   const [isEntreeDropdownOpen, setIsEntreeDropdownOpen] = useState(false);
 //   const authState = useSelector(state => state.auth);
 //   const userState = useSelector(state => state.user);
 //   const userData = useSelector(state => state.user.userData);
-  
 //   const [userRole, setUserRole] = useState(null);
 //   const [findUser, setFindUser] = useState(null);
 
 //   useEffect(() => {
-//     const username = authState.user.username;
-//     const foundUser = userData.find(user => user.login_User === username);
-//     setFindUser(foundUser);
-//     setUserRole(foundUser.type_User);
-//   }, [userData]);
+//     const username = authState.user?.username;
+//     if (username && userData) {
+//       const foundUser = userData.find(user => user.login_User === username);
+//       setFindUser(foundUser);
+//       setUserRole(foundUser?.type_User || null);
+//     }
+//   }, [authState.user, userData]);
 
 //   const toggleEntreeDropdown = () => {
 //     setIsEntreeDropdownOpen(!isEntreeDropdownOpen);
@@ -111,6 +113,14 @@
 //               <span className="text-lg">Sortie</span>
 //             </Link>
 //           </li>
+
+
+//           <li className="relative">
+//             <Link to="/profile" className="flex items-center space-x-3 px-6 py-3 text-white hover:bg-gray-700 rounded-lg transition duration-200">
+//               <CgProfile  className="text-2xl" />
+//               <span className="text-lg">Profile</span>
+//             </Link>
+//           </li>
 //         </ul>
 //       </nav>
 //     </header>
@@ -118,9 +128,7 @@
 // };
 
 // export default SideBare;
-
-
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { TbTruckDelivery, TbTruckReturn, TbArrowBadgeRight } from "react-icons/tb";
@@ -129,47 +137,70 @@ import { FaRegUser } from "react-icons/fa6";
 import { AiOutlineProduct } from "react-icons/ai";
 import { SlBasket, SlArrowDown, SlArrowUp } from "react-icons/sl";
 import { RxDashboard } from "react-icons/rx";
+import { CgProfile } from "react-icons/cg";
 
 const SideBare = () => {
   const [isEntreeDropdownOpen, setIsEntreeDropdownOpen] = useState(false);
+  const [user, setUser] = useState(null);
+  const [foundUser, setFoundUser] = useState(null);
+  const [fullName, setFullName] = useState(null);
+  const [typeUser, setTypeUser] = useState(null);
   const authState = useSelector(state => state.auth);
   const userState = useSelector(state => state.user);
-  const userData = useSelector(state => state.user.userData);
-  
-  const [userRole, setUserRole] = useState(null);
-  const [findUser, setFindUser] = useState(null);
+  useEffect(() => {
+    if (authState.user) {
+      setUser(authState.user);
+    }
+  }, [authState]);
 
   useEffect(() => {
-    const username = authState.user?.username;
-    if (username && userData) {
-      const foundUser = userData.find(user => user.login_User === username);
-      setFindUser(foundUser);
-      setUserRole(foundUser?.type_User || null);
+    if (user && userState.userData.length > 0) {
+      // const match = userState.userData.find(u => u.id == user.id);
+      const match = userState.userData.find(usr => usr.login_User == user.username);
+      console.log("match result", match.type_User)
+      setFoundUser(match.nom_User.slice(0, 1).toUpperCase()+match.prenom_User.slice(0, 1).toUpperCase());
+      setFullName(match.nom_User+' '+match.prenom_User);
+      setTypeUser(match.type_User)
     }
-  }, [authState.user, userData]);
+  }, [user, userState]);
 
+  // console.log("User Role: => ", typeUser)
+  console.log("from sideBare founduser: => ", foundUser)
+  // console.log("from sideBare userState => ", userState)
   const toggleEntreeDropdown = () => {
     setIsEntreeDropdownOpen(!isEntreeDropdownOpen);
   };
+  // typeUser === 'Admin' ||  typeUser === "Super Admin"
+  const checkAccess = ()=>{
+    if (typeUser === "Super Admin") return true
+    else if (typeUser === "Admin") return true
 
-  const userInitials = findUser ? findUser.nom_User.slice(0, 1).toUpperCase() + findUser.prenom_User.slice(0, 1).toUpperCase() : '';
+    else return false
+  }
+  // if (!user || !foundUser) {
+  //   <div>Loading...</div>; 
+  // }
 
+  // const userInitials = foundUser.nom_User.slice(0, 1).toUpperCase() + foundUser.prenom_User.slice(0, 1).toUpperCase();
   return (
     <header className="fixed z-50 md:relative">
       <input type="checkbox" className="peer hidden" id="sidebar-open" />
-  
+      <label className="peer-checked:rounded-full peer-checked:p-2 peer-checked:right-6 peer-checked:bg-gray-600 peer-checked:text-white absolute top-8 z-20 mx-4 cursor-pointer md:hidden" htmlFor="sidebar-open">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+      </label>
       <nav
         aria-label="Sidebar Navigation"
-        className="peer-checked:w-52 left-0 z-10 flex h-screen w-0 flex-col overflow-hidden bg-gray-800 text-white transition-all duration-300 md:h-screen md:w-52 lg:w-52"
+        className="peer-checked:w-52 left-0 z-10 flex h-screen w-0 flex-col overflow-hidden bg-gray-800 text-white transition-all duration-300 md:h-[2000px] md:w-52 lg:w-52"
       >
-      
-     
         <div className="flex flex-col items-center px-6 py-3 space-y-2 bg-gray-700 rounded-lg mt-4">
           <div className="flex items-center justify-center w-12 h-12 bg-gray-500 rounded-full text-xl font-bold">
-            {userInitials}
+            {/* {userInitials} */}
+            {foundUser}
           </div>
           <div className="text-lg text-center">
-            Welcome back, <br /> {findUser ? findUser.prenom_User : ''}
+            Welcome Back, <br /> {fullName}
           </div>
         </div>
         <ul className="mt-8 space-y-3 md:mt-20">
@@ -179,61 +210,75 @@ const SideBare = () => {
               <span className="text-lg">Dashboard</span>
             </Link>
           </li>
-          <li className="relative">
-            <Link to="/users" className="flex items-center space-x-3 px-6 py-3 text-white hover:bg-gray-700 rounded-lg transition duration-200">
-              <FaRegUser className="text-2xl" />
-              <span className="text-lg">Users</span>
-            </Link>
-          </li>
+          {/* {typeUser == "Super Admin" && */}
+          {checkAccess() &&
+            <li className="relative">
+              <Link to="/users" className="flex items-center space-x-3 px-6 py-3 text-white hover:bg-gray-700 rounded-lg transition duration-200">
+                <FaRegUser className="text-2xl" />
+                <span className="text-lg">Users</span>
+              </Link>
+            </li>
+          }
           <li className="relative">
             <Link to="/products" className="flex items-center space-x-3 px-6 py-3 text-white hover:bg-gray-700 rounded-lg transition duration-200">
               <AiOutlineProduct className="text-2xl" />
               <span className="text-lg">Products</span>
             </Link>
           </li>
+          {checkAccess() &&
+            <>
+              <li className="relative">
+                <button onClick={toggleEntreeDropdown} className="flex items-center justify-between w-full px-6 py-3 text-white hover:bg-gray-700 rounded-lg transition duration-200">
+                  <div className="flex items-center space-x-3">
+                    <SlBasket className="text-2xl" />
+                    <span className="text-lg">Entree</span>
+                  </div>
+                  {isEntreeDropdownOpen ? <SlArrowUp className="text-xl" /> : <SlArrowDown className="text-xl" />}
+                </button>
+                {isEntreeDropdownOpen && (
+                  <ul className="mt-2 space-y-2 pl-12">
+                    <li>
+                      <Link to="/liste-demandes" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition duration-200">
+                        <TbArrowBadgeRight className="text-xl" />
+                        <span>ListeDA</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/entree" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition duration-200">
+                        <TbArrowBadgeRight className="text-xl" />
+                        <span>Entree</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/livraison" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition duration-200">
+                        <TbArrowBadgeRight className="text-xl" />
+                        <span>Livraison</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/sortie" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition duration-200">
+                        <TbArrowBadgeRight className="text-xl" />
+                        <span>Return</span>
+                      </Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li className="relative">
+                <Link to="/sortie" className="flex items-center space-x-3 px-6 py-3 text-white hover:bg-gray-700 rounded-lg transition duration-200">
+                  <BiPurchaseTagAlt className="text-2xl" />
+                  <span className="text-lg">Sortie</span>
+                </Link>
+              </li>
+            </>
+          }
           <li className="relative">
-            <button onClick={toggleEntreeDropdown} className="flex items-center justify-between w-full px-6 py-3 text-white hover:bg-gray-700 rounded-lg transition duration-200">
-              <div className="flex items-center space-x-3">
-                <SlBasket className="text-2xl" />
-                <span className="text-lg">Entree</span>
-              </div>
-              {isEntreeDropdownOpen ? <SlArrowUp className="text-xl" /> : <SlArrowDown className="text-xl" />}
-            </button>
-            {isEntreeDropdownOpen && (
-              <ul className="mt-2 space-y-2 pl-12">
-                <li>
-                <Link to="/liste-demandes" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition duration-200">
-                    <TbArrowBadgeRight className="text-xl" />
-                    <span>ListeDA</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/entree" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition duration-200">
-                    <TbArrowBadgeRight className="text-xl" />
-                    <span>entree</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/livraison" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition duration-200">
-                    <TbArrowBadgeRight className="text-xl" />
-                    <span>Livraison</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/sortie" className="flex items-center space-x-2 text-lg text-gray-300 hover:text-white transition duration-200">
-                    <TbArrowBadgeRight className="text-xl" />
-                    <span>Return</span>
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </li>
-          <li className="relative">
-            <Link to="/sortie" className="flex items-center space-x-3 px-6 py-3 text-white hover:bg-gray-700 rounded-lg transition duration-200">
-              <BiPurchaseTagAlt className="text-2xl" />
-              <span className="text-lg">Sortie</span>
+            <Link to="/profile" className="flex items-center space-x-3 px-6 py-3 text-white hover:bg-gray-700 rounded-lg transition duration-200">
+              <CgProfile  className="text-2xl" />
+              <span className="text-lg">Profile</span>
             </Link>
           </li>
+       
         </ul>
       </nav>
     </header>
