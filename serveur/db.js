b6d22d4db9139285d5c04d99fcb6e8efc49c367b
P4ver <s.pales20@gmail.com>
2024-06-13 16:28:13 +0100

    testing for deploying

--- serveur/db.js
+++ serveur/db.js
@@ -1,30 +1,30 @@
-const mysql = require('mysql');
-require('dotenv').config();
-
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
-});
-
-module.exports = pool;
-
-
 // const mysql = require('mysql');
+// require('dotenv').config();
 
 // const pool = mysql.createPool({
 //     connectionLimit: 10,
-//     host: 'bdibawrkccer658nc2x3-mysql.services.clever-cloud.com',
-//     user: 'uwdlyklpge60bjid', // Your MySQL username
-//     password: "a6ETVhHQPzWDQjxOyyjG", // Your MySQL password
-//     database: 'bdibawrkccer658nc2x3', // Name of the database you created
-//     port: 3306
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     port: process.env.DB_PORT
 // });
 
 // module.exports = pool;
+
+
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'bdibawrkccer658nc2x3-mysql.services.clever-cloud.com',
+    user: 'uwdlyklpge60bjid', // Your MySQL username
+    password: "a6ETVhHQPzWDQjxOyyjG", // Your MySQL password
+    database: 'bdibawrkccer658nc2x3', // Name of the database you created
+    port: 3306
+});
+
+module.exports = pool;
 
 
 // const mysql = require('mysql');
@@ -37,4 +37,4 @@ module.exports = pool;
 //     database: 'fadesoldb', // Name of the database you created
 // });
 
-module.exports = pool;
+// module.exports = pool;
