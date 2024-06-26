f01d9a47e0b1400316dc8d78811f330550923aac
P4ver <s.pales20@gmail.com>
2024-06-26 18:02:45 +0100

    ds

--- FrontEnd_Test/src/component/historique.jsx
+++ FrontEnd_Test/src/component/historique.jsx
@@ -1,3 +1,5 @@
+
+
 // import React, { useState, useEffect } from 'react';
 // import { useSelector, useDispatch } from 'react-redux';
 // import { fetchHistoriqueData } from '../store/historiqueSlice';
@@ -7,7 +9,10 @@
 //   const historiqueData = useSelector(state => state.historique.historiqueData);
 //   const authState = useSelector(state => state.auth);
 //   const user = authState.user;
-// // console.log("from historical",historiqueData)
+
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const [filteredData, setFilteredData] = useState([]);
+
 //   useEffect(() => {
 //     dispatch(fetchHistoriqueData());
 
@@ -20,10 +25,29 @@
 //     return () => clearInterval(interval);
 //   }, [dispatch]);
 
-//   const filteredData = historiqueData.filter(data => data.user_Dmd === user.username);
-// console.log("historique: filteredData", filteredData)
+//   useEffect(() => {
+//     // Filter data based on searchTerm
+//     const filtered = historiqueData.filter(item =>
+//       item.user_Dmd && item.user_Dmd.toLowerCase().includes(searchTerm.toLowerCase()) ||
+//       item.date_Op && item.date_Op .toLowerCase().includes(searchTerm.toLowerCase()) ||
+//       item.type_Op && item.type_Op .toLowerCase().includes(searchTerm.toLowerCase())
+//     );
+//     setFilteredData(filtered);
+//   }, [historiqueData, searchTerm]);
+
+//   const handleSearchChange = e => {
+//     setSearchTerm(e.target.value);
+//   };
+
 //   return (
 //     <div className="w-full mx-auto p-4">
+//       <input
+//         type="text"
+//         placeholder="Rechercher par utilisateur, date ou type opération..."
+//         value={searchTerm}
+//         onChange={handleSearchChange}
+//         className="p-2 mb-4 border border-gray-300 rounded"
+//       />
 //       <table className="min-w-full table-auto bg-white border border-gray-200">
 //         <thead>
 //           <tr className='bg-green-600 text-white'>
@@ -40,7 +64,7 @@
 //           </tr>
 //         </thead>
 //         <tbody>
-//           {filteredData.reverse().map((item, index) => (
+//           {filteredData.slice().reverse().map((item, index) => (
 //             <tr key={item.id_Historique} className={index % 2 === 0 ? 'bg-gray-100' : ''}>
 //               <td className="border px-4 ">{item.id_Historique}</td>
 //               <td className="border px-4 ">{item.type_Op}</td>
@@ -63,6 +87,15 @@
 // export default Historique;
 
 
+
+
+
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchHistoriqueData } from '../store/historiqueSlice';
@@ -75,6 +108,8 @@ const Historique = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredData, setFilteredData] = useState([]);
+  const [selectedDate, setSelectedDate] = useState('');
+  const [stats, setStats] = useState({ entrees: 0, sorties: 0, retours: 0 });
 
   useEffect(() => {
     dispatch(fetchHistoriqueData());
@@ -91,63 +126,124 @@ const Historique = () => {
   useEffect(() => {
     // Filter data based on searchTerm
     const filtered = historiqueData.filter(item =>
-      item.user_Dmd && item.user_Dmd.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      item.date_Op && item.date_Op .toLowerCase().includes(searchTerm.toLowerCase()) ||
-      item.type_Op && item.type_Op .toLowerCase().includes(searchTerm.toLowerCase())
+      (item.user_Dmd && item.user_Dmd.toLowerCase().includes(searchTerm.toLowerCase())) ||
+      (item.date_Op && item.date_Op.toLowerCase().includes(searchTerm.toLowerCase())) ||
+      (item.type_Op && item.type_Op.toLowerCase().includes(searchTerm.toLowerCase()))
     );
     setFilteredData(filtered);
   }, [historiqueData, searchTerm]);
+
+  useEffect(() => {
+    // Calculate statistics based on selectedDate
+    const stats = { entrees: 0, sorties: 0, retours: 0 };
+
+    const filteredByDate = historiqueData.filter(item => item.date_Op.includes(selectedDate));
+    
+    filteredByDate.forEach(item => {
+      if (item.type_Op === 'entree') stats.entrees += 1;
+      else if (item.type_Op === 'sortie') stats.sorties += 1;
+      else if (item.type_Op === 'retour') stats.retours += 1;
+    });
+
+    setFilteredData(filteredByDate);
+    setStats(stats);
+  }, [historiqueData, selectedDate]);
 
   const handleSearchChange = e => {
     setSearchTerm(e.target.value);
   };
 
+  const handleDateChange = e => {
+    setSelectedDate(e.target.value);
+  };
+
   return (
-    <div className="w-full mx-auto p-4">
-      <input
-        type="text"
-        placeholder="Rechercher par utilisateur, date ou type opération..."
-        value={searchTerm}
-        onChange={handleSearchChange}
-        className="p-2 mb-4 border border-gray-300 rounded"
-      />
-      <table className="min-w-full table-auto bg-white border border-gray-200">
-        <thead>
-          <tr className='bg-green-600 text-white'>
-            <th className="px-4 py-2">ID</th>
-            <th className="px-4 py-2">Type Operation</th>
-            <th className="px-4 py-2">Date</th>
-            <th className="px-4 py-2">Code Produit</th>
-            <th className="px-4 py-2">Designation</th>
-            <th className="px-4 py-2">Quantite</th>
-            <th className="px-4 py-2">N° Serie</th>
-            <th className="px-4 py-2">Code Projet</th>
-            <th className="px-4 py-2">Nom Projet</th>
-            <th className="px-4 py-2">User</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.slice().reverse().map((item, index) => (
-            <tr key={item.id_Historique} className={index % 2 === 0 ? 'bg-gray-100' : ''}>
-              <td className="border px-4 ">{item.id_Historique}</td>
-              <td className="border px-4 ">{item.type_Op}</td>
-              <td className="border px-4 ">{item.date_Op}</td>
-              <td className="border px-4 ">{item.code_Produit}</td>
-              <td className="border px-4 ">{item.designation_Produit}</td>
-              <td className="border px-4 ">{item.qte_Produit}</td>
-              <td className="border px-4 ">{item.n_Serie}</td>
-              <td className="border px-4 ">{item.code_Projet}</td>
-              <td className="border px-4 ">{item.nom_Projet}</td>
-              <td className="border px-4 ">{item.user_Dmd}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="max-w-7xl mx-auto p-4">
+      <div className="mb-4 flex justify-between items-center">
+        <div className="flex gap-4">
+          <input
+            type="text"
+            placeholder="Rechercher par utilisateur, date ou type opération..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+            className="p-2 mb-4 border border-gray-300 rounded shadow-sm w-full"
+          />
+          <input
+            type="date"
+            value={selectedDate}
+            onChange={handleDateChange}
+            className="p-2 mb-4 border border-gray-300 rounded shadow-sm w-[250px]"
+          />
+        </div>
+      </div>
+      {selectedDate && (
+        <div className="mb-4 grid grid-cols-3 gap-4">
+          <div className="bg-blue-500 text-white p-4 rounded-lg shadow-md">
+            <h3 className="text-xl font-bold">Entrées</h3>
+            <p className="text-2xl">{stats.entrees}</p>
+          </div>
+          <div className="bg-red-500 text-white p-4 rounded-lg shadow-md">
+            <h3 className="text-xl font-bold">Sorties</h3>
+            <p className="text-2xl">{stats.sorties}</p>
+          </div>
+          <div className="bg-yellow-500 text-white p-4 rounded-lg shadow-md">
+            <h3 className="text-xl font-bold">Retours</h3>
+            <p className="text-2xl">{stats.retours}</p>
+          </div>
+        </div>
+      )}
+      {selectedDate && filteredData.length > 0 && (
+        <div className="overflow-x-auto">
+          <table className="min-w-full table-auto bg-white border border-gray-200">
+            <thead>
+              <tr className="bg-green-600 text-white">
+                {/* <th className="px-4 py-2">ID</th> */}
+                <th className="px-4 py-2">Type Operation</th>
+                <th className="px-4 py-2">Date</th>
+                <th className="px-4 py-2">Code Produit</th>
+                <th className="px-4 py-2">Designation</th>
+                <th className="px-4 py-2">Quantite</th>
+                <th className="px-4 py-2">N° Serie</th>
+                <th className="px-4 py-2">Code Projet</th>
+                <th className="px-4 py-2">Nom Projet</th>
+                <th className="px-4 py-2">User</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredData.slice().reverse().map((item, index) => (
+                <tr key={item.id_Historique} className={index % 2 === 0 ? 'bg-gray-100' : ''}>
+                  {/* <td className="border px-4 py-2">{item.id_Historique}</td> */}
+                  <td className="border px-4 py-2">{item.type_Op}</td>
+                  <td className="border px-4 py-2">{item.date_Op}</td>
+                  <td className="border px-4 py-2">{item.code_Produit}</td>
+                  <td className="border px-4 py-2">{item.designation_Produit}</td>
+                  <td className="border px-4 py-2">{item.qte_Produit}</td>
+                  <td className="border px-4 py-2">{item.n_Serie}</td>
+                  <td className="border px-4 py-2">{item.code_Projet}</td>
+                  <td className="border px-4 py-2">{item.nom_Projet}</td>
+                  <td className="border px-4 py-2">{item.user_Dmd}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Historique;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
