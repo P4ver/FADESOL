cba8f6fcd7e0a822fa207107476bdbe49d397cf0
P4ver <s.pales20@gmail.com>
2024-08-08 11:41:06 +0100

    return

--- FrontEnd_Test/src/component/listeReturn.jsx
+++ FrontEnd_Test/src/component/listeReturn.jsx
@@ -1,23 +1,17 @@
-
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchAchatempoData, updateAchatempoData, deleteAchatempoData } from '../store/achatempoSlice';
-// import {fetchProductData,updateProductData} from "../store/productSlice"
-import {fetchProductData,updateProductData, updateQteMagasin} from "../store/productSlice"
-import Modal from 'react-modal';
-import { FaEye, FaPencilAlt, FaCheck, FaTimes, FaTruck, FaPrint } from 'react-icons/fa';
-import {
-  Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
-  Paper, IconButton, TextField, Button, Typography, Box
-} from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { confirmAlert } from 'react-confirm-alert';
-import 'react-confirm-alert/src/react-confirm-alert.css';
-import logo from '../pictures/logo.png';
-import { fetchAchatData, postAchatData, deleteAchatData } from '../store/achatSlice';
+import { fetchProductData, updateProductData, updateQteMagasin } from '../store/productSlice';
+import { fetchProjetData } from '../store/projetSlice';
+import { fetchHistoriqueData } from '../store/historiqueSlice';
+import { fetchClientData } from '../store/clientSlice';
 import { fetchVenteData } from '../store/venteSlice';
-import { fetchReturnData } from '../store/returnSlice'; 
-
+import { fetchReturnData, postReturnData, deleteReturnData } from '../store/returnSlice';
+import Modal from 'react-modal';
+import { FaEye, FaPrint, FaTimes } from 'react-icons/fa';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton, Typography, Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import logo from '../pictures/logo.png';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 Modal.setAppElement('#root');
 
@@ -43,101 +37,52 @@ const useStyles = makeStyles({
     alignItems: 'center',
     marginBottom: 20,
   },
-  input: {
-    marginBottom: '10px',
-    width: '100px',
-  },
-  updateButton: {
-    marginLeft: 10,
-  },
-  statusIcon: {
-    marginRight: 5,
-    verticalAlign: 'middle',
-  },
-  validateButton: {
-    marginTop: 10,
+  printArea: {
+    display: 'none', // Masquer l'élément par défaut
   },
   printArea: {
     display: 'none',
-  },
-  filterContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  searchInput: {
-    marginRight: 10,
-
   },
 });
 
 function ListeReturn() {
   const classes = useStyles();
-  const { achatempoData } = useSelector(state => state.achatempo);
-  const { achatData } = useSelector(state => state.achat);
+  const dispatch = useDispatch();
   const { productData } = useSelector(state => state.product);
   const { venteData } = useSelector(state => state.vente);
   const { returnData } = useSelector(state => state.return);
   const authState = useSelector(state => state.auth);
   const user = authState.user;
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchAchatData());
-    dispatch(fetchProductData());
-    dispatch(fetchAchatempoData());
-    dispatch(fetchVenteData());
-  }, [dispatch]);
+
   const [searchQuery, setSearchQuery] = useState('');
   const [filterType, setFilterType] = useState('all');
-  const [qteRecu, setQteRecu] = useState({});
-  const [updateSuccess, setUpdateSuccess] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [selectedAchat, setSelectedAchat] = useState(null);
-
-  const filteredAchatData = achatempoData.filter(data => data.user_Dmd === user.username);
-  const filteredVenteData = venteData.filter(data => data.user_Dmd === user.username);
-//   console.log("filteredVenteData***:",filteredVenteData)
-
+  const [selectedReturn, setSelectedReturn] = useState(null);
 
   useEffect(() => {
-    if (updateSuccess) {
-      // window.location.reload();
-      console.log("avoid reload")
-    }
-  }, [updateSuccess]);
+    dispatch(fetchReturnData());
+    dispatch(fetchProductData());
+    dispatch(fetchVenteData());
+  }, [dispatch]);
 
-  const openModal = (achat) => {
-    setSelectedAchat(achat);
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-    setSelectedAchat(null);
-  };
-
-  // const uniqueCodeAchats = [...new Set(filteredAchatData.map(data => data.code_Achat))];
-  const uniqueCodeVente = [...new Set(returnData.map(data => data.code_Sortie))]
-
-  console.log("fronSOrtX",uniqueCodeVente)
+  const uniqueCodeVente = [...new Set(returnData.map(data => data.code_Return))];
 
   useEffect(() => {
     const handleDeleteDuplicates = async () => {
       try {
-        const achatMap = {};
+        const returnMap = {};
         const duplicates = [];
   
-        achatData.forEach(achat => {
-          const key = `${achat.code}-${achat.code_Projet}-${achat.designation}-${achat.quantite}-${achat.nom_Projet}-${achat.date}-${achat.code_Achat}-${achat.user_Dmd}`;
-          if (achatMap[key]) {
-            duplicates.push(achat.id_Achat);
+        returnData.forEach(returnItem => {
+          const key = `${returnItem.code}-${returnItem.code_Projet}-${returnItem.designation}-${returnItem.quantite}-${returnItem.nom_Projet}-${returnItem.date}-${returnItem.code_Return}-${returnItem.user_Dmd}`;
+          if (returnMap[key]) {
+            duplicates.push(returnItem.id_return);
           } else {
-            achatMap[key] = true;
+            returnMap[key] = true;
           }
         });
   
-        const deletePromises = duplicates.map(id => dispatch(deleteAchatData(id)));
+        const deletePromises = duplicates.map(id => dispatch(deleteReturnData(id)));
         await Promise.all(deletePromises);
       } catch (error) {
         console.error('Error deleting duplicates:', error);
@@ -146,62 +91,68 @@ function ListeReturn() {
     };
   
     handleDeleteDuplicates();
-  }, [achatData, qteRecu]);
+  }, [returnData, dispatch]);
 
-  const handlePrint = () => {
-    const printContents = document.getElementById('print-area').innerHTML;
-    const originalContents = document.body.innerHTML;
-    document.body.innerHTML = printContents;
-    window.print();
-    document.body.innerHTML = originalContents;
-    window.location.reload();
+  const openModal = (returnItem) => {
+    setSelectedReturn(returnItem);
+    setModalIsOpen(true);
   };
 
-  const getGeneralStatus = (codeAchat) => {
-    const relatedDemands = filteredAchatData.filter(data => data.code_Achat === codeAchat);
-
-    if (relatedDemands.every(demand => demand.qte_Reçu === 0)) {
-      return 'Pending';
-    }
-
-    if (relatedDemands.every(demand => demand.qte_Reçu === demand.quantite)) {
-      return 'Livré';
-    }
-
-    if (relatedDemands.some(demand => demand.qte_Reçu > 0 && demand.qte_Reçu < demand.quantite)) {
-      return 'Partiellement livré';
-    }
-
-    return 'Unknown';
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setSelectedReturn(null);
   };
 
-  const handleSearch = (event) => {
-    setSearchQuery(event.target.value);
-  };
+ const getGeneralStatus = (codeReturn) => {
+  const relatedDemands = returnData.filter(data => data.code_Return === codeReturn);
 
-  const handleFilterChange = (event) => {
-    setFilterType(event.target.value);
-  };
+  if (relatedDemands.every(demand => demand.qteRecue === 0)) {
+    return 'Pending';
+  }
 
-  const filteredAndSearchedData = filteredAchatData.filter((data) => {
-    const matchesSearchQuery = data.code_Achat.toLowerCase().includes(searchQuery.toLowerCase());
+  if (relatedDemands.every(demand => demand.qteRecue === demand.quantite)) {
+    return 'Livré';
+  }
+
+  if (relatedDemands.some(demand => demand.qteRecue > 0 && demand.qteRecue < demand.quantite)) {
+    return 'Partiellement livré';
+  }
+
+  return 'Unknown';
+};
+
+
+const handlePrint = () => {
+  const printContents = document.getElementById('print-area').innerHTML;
+  const originalContents = document.body.innerHTML;
+  document.body.innerHTML = printContents;
+  window.print();
+  document.body.innerHTML = originalContents;
+  window.location.reload();
+};
+  
+  const filteredReturnData = returnData.filter(data => {
+    const codeReturn = data.code_Return || ''; // Valeur par défaut si undefined
+    const query = searchQuery || ''; // Valeur par défaut si undefined
+  
+    const matchesSearchQuery = codeReturn.toLowerCase().includes(query.toLowerCase());
+    const status = getGeneralStatus(codeReturn).toLowerCase(); // Assurez-vous que getGeneralStatus renvoie une chaîne non vide
+  
     const matchesFilterType =
       filterType === 'all' ||
-      (filterType === 'livre' && getGeneralStatus(data.code_Achat) === 'Livré') ||
-      (filterType === 'partiellement_livre' && getGeneralStatus(data.code_Achat) === 'Partiellement livré') ||
-      (filterType === 'pending' && getGeneralStatus(data.code_Achat) === 'Pending');
-
+      (filterType === 'livre' && status === 'livré') ||
+      (filterType === 'partiellement_livre' && status === 'partiellement livré') ||
+      (filterType === 'pending' && status === 'pending');
+  
     return matchesSearchQuery && matchesFilterType;
   });
-
-
+  
   const currentDate = new Date();
   const formattedDate = currentDate.toISOString().slice(0, 10); // Extract yyyy-mm-dd part
-  
-// console.log("from listdemand:",achatempoData)
+
   return (
     <div>
-      <Typography variant="h5" gutterBottom>Liste des Demandes de Sortie</Typography>
+      <Typography variant="h5" gutterBottom>Liste des Demandes de Return</Typography>
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="simple table" size='small'>
           <TableHead>
@@ -209,92 +160,121 @@ function ListeReturn() {
               <TableCell>ID</TableCell>
               <TableCell>Date</TableCell>
               <TableCell>Utilisateur</TableCell>
-              {/* <TableCell>Status</TableCell> */}
               <TableCell>Actions</TableCell>
             </TableRow>
           </TableHead>
-       
           <TableBody>
-  {uniqueCodeVente.map((codeAchat) => {
-    const relatedDemands = returnData.filter(data => data.code_Sortie === codeAchat);
-    if (relatedDemands.length === 0) return null; // Skip if no matching demands
-    const firstDemand = relatedDemands[0];
-    const status = getGeneralStatus(codeAchat);
-    return (
-      <React.Fragment key={codeAchat}>
-        <TableRow>
-          <TableCell>{firstDemand.code_Sortie}</TableCell>
-          <TableCell>{formattedDate}</TableCell>
-          <TableCell>{firstDemand.user_Dmd}</TableCell>
-          {/* <TableCell>{renderStatus(status)}</TableCell> Use renderStatus to display the status with the correct styling */}
-          <TableCell>
-            <IconButton onClick={() => openModal(firstDemand)}><div className='text-blue-500'><FaEye /></div></IconButton>
-            {/* <IconButton onClick={() => handleDelete(firstDemand.id_Achat)}><div className='text-red-500'><FaTimes /></div></IconButton> */}
-          </TableCell>
-        </TableRow>
-      </React.Fragment>
-    );
-  })}
-</TableBody>
+            {uniqueCodeVente.map((codeReturn) => {
+              const relatedDemands = returnData.filter(data => data.code_Return === codeReturn);
+              if (relatedDemands.length === 0) return null; // Skip if no matching demands
+              const firstDemand = relatedDemands[0];
+              const status = getGeneralStatus(codeReturn);
+              return (
+                <TableRow key={codeReturn}>
+                  <TableCell>{firstDemand.code_Return}</TableCell>
+                  <TableCell>{formattedDate}</TableCell>
+                  <TableCell>{firstDemand.user_Dmd}</TableCell>
+                  <TableCell>
+                    <IconButton onClick={() => openModal(firstDemand)}><FaEye /></IconButton>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
         </Table>
       </TableContainer>
 
       <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        className={classes.modal}
-        contentLabel="Order Details"
+  isOpen={modalIsOpen}
+  onRequestClose={closeModal}
+  className={classes.modal}
+  contentLabel="Order Details"
+>
+  <div className={classes.modalHeader}>
+    <Typography variant="h6">Details de la Demande de Return</Typography>
+    <IconButton onClick={closeModal}><FaTimes /></IconButton>
+  </div>
+  {selectedReturn && (
+    <>
+      <Typography variant="subtitle1">Code Sortie: {selectedReturn.code_Return}</Typography>
+      <Typography variant="subtitle1">Date: {selectedReturn.date_Vente}</Typography>
+      <Typography variant="subtitle1">Utilisateur: {selectedReturn.user_Dmd}</Typography>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Désignation</TableCell>
+              <TableCell>Quantité Demandée</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {returnData.filter(data => data.code_Return === selectedReturn.code_Return).map((data) => (
+              <TableRow key={data.id_Vente}>
+                <TableCell>{data.designation_Produit}</TableCell>
+                <TableCell>{data.qte_Produit}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={handlePrint}
       >
-        <div className={classes.modalHeader}>
-          <Typography variant="h6">Details de la Demande de Sortie</Typography>
-          <IconButton onClick={closeModal}><FaTimes /></IconButton>
-        </div>
-        {selectedAchat && (
-          <>
-            <Typography variant="subtitle1">Code Sortie: {selectedAchat.code_Sortie}</Typography>
-            <Typography variant="subtitle1">Date: {selectedAchat.date_Vente}</Typography>
-            <Typography variant="subtitle1">Utilisateur: {selectedAchat.user_Dmd}</Typography>
-            <TableContainer component={Paper}>
-              <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Désignation</TableCell>
-                    <TableCell>Quantité Demandée</TableCell>
-                    {/* <TableCell>Qte Magasin</TableCell> */}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {returnData.filter(data => data.code_Sortie === selectedAchat.code_Sortie).map((data) => (
-                    <TableRow key={data.id_Vente}>
-                      {/* <TableCell>{data.code_Projet}</TableCell> */}
-                      <TableCell>{data.designation_Produit}</TableCell>
-                      <TableCell>{data.qte_Produit}</TableCell>
-                      {/* <TableCell>{lookNewQteMagasin(data.id_Article)}</TableCell> */}
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <Button
-              variant="contained"
-              color="secondary"
-              className={classes.updateButton}
-              onClick={handlePrint}
-            >
-              <FaPrint /> Imprimer
-            </Button>
-            <div id="print-area" className={`${classes.printArea}`}>
+        <FaPrint /> Imprimer
+      </Button>
+    {/* </>
+  )}
+</Modal>
+
+<div id="print-area" className={classes.printArea}>
   <div className='w-32 mx-auto'>
     <img src={logo} alt="Logo" />
   </div>
-  <h5 className='mt-4'>Demande de Sortie</h5>
+  <Typography variant="h5" gutterBottom>Liste des Demandes de Return</Typography>
+  <TableContainer component={Paper}>
+    <Table className={classes.table} aria-label="simple table" size='small'>
+      <TableHead>
+        <TableRow>
+          <TableCell>ID</TableCell>
+          <TableCell>Date</TableCell>
+          <TableCell>Utilisateur</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {uniqueCodeVente.map((codeReturn) => {
+          const relatedDemands = returnData.filter(data => data.code_Return === codeReturn);
+          if (relatedDemands.length === 0) return null;
+          const firstDemand = relatedDemands[0];
+          return (
+            <TableRow key={codeReturn}>
+              <TableCell>{firstDemand.code_Return}</TableCell>
+              <TableCell>{formattedDate}</TableCell>
+              <TableCell>{firstDemand.user_Dmd}</TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  </TableContainer>
+</div>
+
+    </div>
+  );
+} */}
+      <div id="print-area" className={`${classes.printArea}`}>
+  <div className='w-32 mx-auto'>
+    <img src={logo} alt="Logo" />
+  </div>
+  <h5 className='mt-4'>Demande de Return</h5>
 
   <table className='w-2/5 shadow-y-lg'> 
     <tbody>
       {[
-        { label: 'Code Sortie', value: selectedAchat?.code_Sortie },
-        { label: 'Date', value: selectedAchat?.date_Vente ? new Date(selectedAchat.date_Vente).toISOString().split('T')[0] : '' },
-        { label: 'User', value: selectedAchat?.user_Dmd }
+        { label: 'Code Return', value: selectedReturn?.code_Return },
+        { label: 'Date', value: selectedReturn?.date_Return ? new Date(selectedReturn.date_Return).toISOString().split('T')[0] : '' },
+        { label: 'User', value: selectedReturn?.user_Dmd }
       ].map((item, idx) => (
         <tr key={idx}>
           <td><h6>{item.label}</h6></td>
@@ -312,7 +292,7 @@ function ListeReturn() {
       <tr className='border'>
       <th className="border border-black text-[9px] font-semibold text-center py-1">Code</th>
            <th className="border border-black text-[9px] font-semibold text-center py-1 w-2/5">Désignation</th>
-           <th className="border border-black text-[9px] font-semibold text-center py-1 w-2/5">Client</th>
+           
            <th className="border border-black text-[9px] font-semibold text-center py-1 w-1/5">Quantité</th>
            {/* <th className="border border-black text-[9px] font-semibold text-center py-1 w-1/5">Qte Magasin</th> */}
           {/* <th className="border border-black text-[9px] font-semibold text-center py-1 w-1/5">Projet</th> */}
@@ -320,11 +300,11 @@ function ListeReturn() {
     
     </thead>
     <tbody>
-      {returnData.filter(a => a.code_Sortie === selectedAchat?.code_Sortie).map((item, idx) => (
+      {returnData.filter(a => a.code_Return === selectedReturn?.code_Return).map((item, idx) => (
         <tr key={idx} className='border'>
           <td className=" border border-black text-[9px] text-center py-1 min-w-28">{item.code_Produit}</td>
           <td className=" border border-black text-[9px] text-center  py-1 w-2/5">{item.designation_Produit}</td>
-          <td className=" border border-black text-[9px] text-center  py-1 w-2/5">{item.Partenaire}</td>
+          
           <td className=" border border-black text-[9px] text-center py-1 w-1/5">{item.qte_Produit}</td>
           {/* <td className=" border border-black text-[9px] text-center py-1 w-1/5">{item.qte_Magasin}</td> */}
           {/* <td className=" border border-black text-[9px] text-center py-1 w-1/5">{lookNewQteMagasin(item.id_Article)}</td> */}
@@ -343,10 +323,7 @@ function ListeReturn() {
     </div>
   );
 }
-
 export default ListeReturn;
-
-
 
 //resolve the problem of lower case i think
 // import React, { useState, useEffect } from 'react';
