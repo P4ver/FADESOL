185737f9f28382863b288cff20ac79d947d63933
P4ver <s.pales20@gmail.com>
2024-09-11 17:29:31 +0100

    ad

--- FrontEnd_Test/src/apiConfig.jsx
+++ FrontEnd_Test/src/apiConfig.jsx
@@ -4,8 +4,8 @@
 // export const API_BASE_URL = 'http://localhost:3000';
 // require('dotenv').config();
 // export const API_BASE_URL = process.env.API_BASE
-export const API_BASE_URL = 'https://test-habti.xyz';
-// export const API_BASE_URL = 'http://localhost:3000';
+// export const API_BASE_URL = 'https://test-habti.xyz';
+export const API_BASE_URL = 'https://gspr.website';
 
 // export const API_BASE_URL = 'http://15.236.46.59:3000';
 // export const API_BASE_URL = 'https://fadesol.onrender.com';
