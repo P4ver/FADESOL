6189d2dc336cd099c9df8f09ed4d13a4b5056144
P4ver <s.pales20@gmail.com>
2024-09-19 09:21:50 +0100

    fix counter

--- FrontEnd_Test/src/component/entree.jsx
+++ FrontEnd_Test/src/component/entree.jsx
@@ -146,19 +146,20 @@ const Entree = () => {
     // setCodeAchat(newCodeSortie);
     if (achatempoData && achatempoData.length > 0) {
       // const lastCodeSortie = venteData[venteData.length - 1].code_Sortie;
-      const lastCodeSortie = achatempoData[achatempoData.length - 1].code_Sortie || localStorage.getItem('lastCodeAchat');
-      const lastCodeSortieINT = parseInt(lastCodeSortie.split('-')[1], 10) + 1;
-      const newCodeSortie = `CE-${String(lastCodeSortieINT).padStart(6, '0')}`;
-      console.log("codeEntreeINT code Entree:", newCodeSortie);
-      setCodeAchat(newCodeSortie);
-      localStorage.setItem('lastCodeAchat', newCodeSortie);
+      const lastCodeEntree = achatempoData[achatempoData.length - 1].code_Achat || localStorage.getItem('lastCodeAchat');
+      const lastCodeEntreeINT = parseInt(lastCodeEntree.split('-')[1], 10) + 1;
+      const newCodeEntree = `CE-${String(lastCodeEntreeINT).padStart(6, '0')}`;
+      console.log("codeEntreeINT code Entree:", newCodeEntree);
+      setCodeAchat(newCodeEntree);
+      localStorage.setItem('lastCodeAchat', newCodeEntree);
     } else {
       console.log("achatempoData is empty or undefined.");
       // Handle cases where venteData is empty, e.g., set a default value
       // setCodeAchat("CS-000001");
     }
   };
-
+console.log("achatempoData.length", achatempoData.length)
+// console.log("lastCodeEntree", lastCodeEntree)
   // console.log("ooooouseroooo",user.username)
   // console.log("===>historiqueData==>:", historiqueData)
   const historiqueForUser = historiqueData.filter(historic => historic.user_Dmd === user.username)
